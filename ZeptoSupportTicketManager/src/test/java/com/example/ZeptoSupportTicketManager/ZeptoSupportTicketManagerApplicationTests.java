8bb6486a8c545a43f2b093085fd5ed6a3b50acba
MaanavRajput12 <maanavrajputdr16@gmail.com>
2026-08-08 10:40:08 +0530

    Initial backend commit

---
+++ ZeptoSupportTicketManager/src/test/java/com/example/ZeptoSupportTicketManager/ZeptoSupportTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZeptoSupportTicketManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeptoSupportTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
